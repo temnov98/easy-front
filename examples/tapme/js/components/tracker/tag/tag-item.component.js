4df461b030a57e7523efbb33c5654d22a02f0774
Temnov Aleksey <temnov.aleksei@gmail.com>
2023-12-05 00:38:12 +0300

    Improve user experience with tags

--- examples/tapme/js/components/tracker/tag/tag-item.component.js
+++ examples/tapme/js/components/tracker/tag/tag-item.component.js
@@ -21,7 +21,7 @@ class TagItemComponent extends AutoSubscribeComponent {
 
     toHtml() {
         return t`
-            <div class="row">
+            <div class="row tag-item-component-container">
                 <input
                     class="tag-item-component-input"
                     id="${this.inputId}"
@@ -32,10 +32,19 @@ class TagItemComponent extends AutoSubscribeComponent {
                 <div
                     class="tag-item-component-text"
                     style="background-color: ${this.tag.color}"
-                    onclick="${() => pageModel.deleteTag(pageModel, this.tag)}"
                 >
                     ${this.tag.title}
                 </div>
+                <button
+                    class="icon-button icon-button--red tag-item-component-delete-button"
+                    onclick="${() => pageModel.deleteTag(pageModel, this.tag)}"
+                >
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg">
+                        <g id="trash_24">
+                            <path id="Vector" d="M17 6H22V8H20V21C20 21.2652 19.8946 21.5196 19.7071 21.7071C19.5196 21.8946 19.2652 22 19 22H5C4.73478 22 4.48043 21.8946 4.29289 21.7071C4.10536 21.5196 4 21.2652 4 21V8H2V6H7V3C7 2.73478 7.10536 2.48043 7.29289 2.29289C7.48043 2.10536 7.73478 2 8 2H16C16.2652 2 16.5196 2.10536 16.7071 2.29289C16.8946 2.48043 17 2.73478 17 3V6ZM18 8H6V20H18V8ZM9 4V6H15V4H9Z" />
+                        </g>
+                    </svg>
+                </button>
             </div>
         `;
     }
