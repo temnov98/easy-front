7c020dbb4616330164410baaf181dd5effb638ba
Temnov Aleksey <temnov.aleksei@gmail.com>
2023-12-04 23:32:12 +0300

    Improve user experience with modal window

--- examples/tapme/js/components/tracker/shared/modal-window.component.js
+++ examples/tapme/js/components/tracker/shared/modal-window.component.js
@@ -28,10 +28,34 @@ class DeleteActiveTasksComponent extends Component {
 }
 
 class ModalWindowComponent extends AutoSubscribeComponent {
+    constructor() {
+        super();
+
+        document.addEventListener('keydown', (event) => this.onKeyDown(event));
+    }
+
+    onKeyDown(event) {
+        if (!event || !pageModel.modalWindowType) {
+            return;
+        }
+
+        const key = event.key;
+
+        if (key === 'Escape') {
+            pageModel.modalWindowType = undefined;
+        }
+    }
+
+    onClick(event) {
+        if (event?.srcElement?.className === 'modal-window') {
+            pageModel.modalWindowType = undefined;
+        }
+    }
+
     toHtml() {
         if (pageModel.modalWindowType === 'delete-active-tasks') {
             return t`
-                <div class="modal-window">
+                <div class="modal-window" onclick="${(event) => this.onClick(event)}">
                     ${DeleteActiveTasksComponent}
                 </div>
             `;
