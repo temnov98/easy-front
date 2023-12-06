0784167626a5d4baad79902a68297242e86d9d00
Temnov Aleksey <temnov.aleksei@gmail.com>
2023-12-07 00:17:58 +0300

    Refactor

--- examples/tapme/js/components/shared/debug.component.js
+++ examples/tapme/js/components/shared/debug.component.js
@@ -1,3 +1,34 @@
+class EasyFrontDebugComponent extends AutoSubscribeComponent {
+    constructor() {
+        super();
+
+        this.timer = setInterval(() => this.redraw(), 1000);
+    }
+
+    onDestroy() {
+        clearInterval(this.timer);
+    }
+
+    toHtml() {
+        return t`
+            <div class="none">
+                <div class="debug-components-count">
+                    Components count: ${_idToComponentMapping.size}
+                </div>
+                <div class="debug-handlers-count">
+                    Handlers count: ${_globalFunctions.size}
+                </div>
+                 <div class="debug-warnings-count">
+                    Warnings count: ${_logger.warningsCount}
+                </div>
+                <div class="debug-errors-count">
+                    Errors count: ${_logger.errorsCount}
+                </div>
+            </div>
+        `;
+    }
+}
+
 class DebugComponent extends Component {
     constructor() {
         super();
